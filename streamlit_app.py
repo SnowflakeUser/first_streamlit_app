314c9d1b41a9551dfacf2e53a2a684d8ffae48a5
SnowflakeUser <121128593+SnowflakeUser@users.noreply.github.com>
2022-12-20 16:31:12 -0500

    Update streamlit_app.py
    imported pandas and added table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -6,3 +7,7 @@ streamlit.text('Blueberry Oatmeal')
 streamlit.text('Kale Smoothie')
 streamlit.text('Free Range Eggs')
 streamlit.text('Avocado Toast')
+
+streamlit.title('Build you own fruit smoothie')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
