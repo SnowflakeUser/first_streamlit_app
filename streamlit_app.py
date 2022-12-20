0cdc1cf31ecd08555f72621ef1f4eebb747b6303
SnowflakeUser <121128593+SnowflakeUser@users.noreply.github.com>
2022-12-20 15:43:45 -0500

    Update streamlit_app.py
    Added items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry Oatmeal')
+streamlit.text('Kale Smoothie')
+streamlit.text('Free Range Eggs')
